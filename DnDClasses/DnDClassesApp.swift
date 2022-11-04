9bd3c47d9e8e73c25ad53644a8d7c2266110cc0f
Samueltse37 <samuelt37@gmail.com>
2022-11-04 01:00:34 -0700

    Initial Commit

---
+++ DnDClasses/DnDClassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDClassesApp.swift
+//  DnDClasses
+//
+//  Created by Samuel Tse on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DnDClassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
