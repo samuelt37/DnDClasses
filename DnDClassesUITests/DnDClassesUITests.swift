2b0834c9cfdfbb02f0af9b407ae7f274af0a9450
Samueltse37 <samuelt37@gmail.com>
2022-11-08 18:10:37 -0800

    Revert "ui test"
    This reverts commit 1399a3eeede9bcdc22d78d71863a569eb8b0f7de.

--- DnDClassesUITests/DnDClassesUITests.swift
+++ DnDClassesUITests/DnDClassesUITests.swift
@@ -10,37 +10,24 @@ import XCTest
 class DnDClassesUITests: XCTestCase {
 
     override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
+        // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-        
-        let navigationTitle = app.navigationBars["Classes"].staticTexts["Classes"]
-        XCTAssertTrue(navigationTitle.exists)
-        navigationTitle.tap()
-        
-        let tablesQuery = app.tables
-        XCTAssertTrue(tablesQuery.element.exists)
-        
-        let barbarianbutton = tablesQuery.buttons["Barbarian"]
-        XCTAssertTrue(barbarianbutton.exists)
-        barbarianbutton.tap()
-        
-        let barbResult = app.staticTexts["No spells found."]
-        XCTAssertTrue(barbResult.exists)
-        barbResult.tap()
-        
-        let barbTitle = app.navigationBars["Barbarian Spells"].buttons["Classes"]
-        XCTAssertTrue(barbTitle.exists)
-        barbTitle.tap()
-        
-        let bardButton = tablesQuery/*@START_MENU_TOKEN@*/.buttons["Bard"]/*[[".cells[\"Bard\"].buttons[\"Bard\"]",".buttons[\"Bard\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        XCTAssert(bardButton.exists)
-        bardButton.tap()
-        
-//        tablesQuery.cells["Light, You touch one object that is no larger than 10 feet in any dimension. Until the spell ends, the object sheds bright light in a 20-foot radius and dim light for an additional 20 feet. The light can be colored as you like. Completely covering the object with something opaque blocks the light. The spell ends if you cast it again or dismiss it as an action.\nIf you target an object held or worn by a hostile creature, that creature must succeed on a dexterity saving throw to avoid the spell."].children(matching: .other).element(boundBy: 0).children(matching: .other).element.tap()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
     func testLaunchPerformance() throws {
